05804dcf743065b7bea1e7bd55e3230e2172c040
365saiom <365saiom@gmail.com>
2023-11-21 15:52:45 +0900

    JSON 형식의 lastResult 데이터 localStorage에 저장

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,8 +25,10 @@ class App {
         api.fetchCats(keyword).then(({ data }) => {
           this.setState(data);
           // console.log("hide");
-          this.Loading.hide();
           // 로딩 hide
+          this.Loading.hide();
+          //로컬에 저장
+          this.saveResult(data);
         });
       },
       onRandomSearch: () => {
@@ -62,11 +64,29 @@ class App {
         image: null,
       },
     });
+    this.init();
   }
 
   setState(nextData) {
     // console.log(this);
     this.data = nextData;
     this.searchResult.setState(nextData);
+  }
+
+  saveResult(result) {
+    console.log(result);
+
+    //JSON을 string으로 바꿀 때, stringfy사용
+    localStorage.setItem("lastResult", JSON.stringify(result));
+  }
+
+  init() {
+    //JSON에 저장된 것을 자바스크립트에서 사용할 수 있는 배열로 바꿀 때, parse 사용
+    const lastResult =
+      localStorage.getItem("lastResult") === null
+        ? []
+        : JSON.parse(localStorage.getItem("lastResult"));
+    console.log(lastResult);
+    this.setState(lastResult);
   }
 }
